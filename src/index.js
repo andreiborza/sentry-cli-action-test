325ca4378cb5ae9805fc5bca6499b86699c0f127
Andrei Borza <andrei.borza@sentry.io>
2025-02-03 14:54:01 +0100

    Test

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import SentryCli from "@sentry/cli";
 
 try {
   const cli = new SentryCli()
-  const inputs = core.getInput('INPUTS');
-  console.log('actions inputs', inputs)
-  const sentryMsg = inputs.sentry_msg
+  const sentryMsg = core.getInput('sentry_msg');
   console.log(`Sending event to Sentry: ${sentryMsg}`)
   cli.execute(['send-event', '-m', sentryMsg], true)
 } catch (error) {
