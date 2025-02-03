fb954a080c77be2118fc2cc5363e63176515b0ff
Andrei Borza <andrei.borza@sentry.io>
2025-02-03 14:37:14 +0100

    Try with inputs

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import SentryCli from "@sentry/cli";
 
 try {
   const cli = new SentryCli()
-  const sentryMsg = core.getInput('sentry_msg');
+  const inputs = core.getInput('INPUTS');
+  console.log('actions inputs', inputs)
+  const sentryMsg = inputs.sentry_msg
   console.log(`Sending event to Sentry: ${sentryMsg}`)
   cli.execute(['send-event', '-m', sentryMsg], true)
 } catch (error) {
