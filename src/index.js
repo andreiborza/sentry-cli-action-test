e9225c8e89168fe666d9d1eb6db906a3bf346474
Andrei Borza <andrei.borza@sentry.io>
2025-02-05 11:33:50 +0100

    Test cwd

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import SentryCli from "@sentry/cli";
 
 try {
   const cli = new SentryCli()
+
+  const workingDirectory = core.getInput('working_directory')
+
+  const currentWorkingDirectory = process.cwd();
+  if (workingDirectory !== null && workingDirectory.length > 0) {
+    process.chdir(workingDirectory);
+  }
+
   const sentryMsg = core.getInput('sentry_msg');
   const cacheHit = process.env['CACHE_HIT'];
   console.log('Got cache hit: ', cacheHit)
